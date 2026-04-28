b9b6d7669fc38888f1536f15325cbb675f148481
Ice_Rink <15551057621@163.com>
2026-04-28 16:00:34 +0000

    docs: 优化邮件配置和文档内容

--- src/module/Core/Mail.js
+++ src/module/Core/Mail.js
@@ -1,22 +1,35 @@
 import { config } from "../../../Config/config.js";
 import nodemailer from "nodemailer";
 
-const transporter = nodemailer.createTransport({
-    host: config.Mail.host,
-    port: config.Mail.port,
-    secure: config.Mail.secure,
-    auth: {
-        user: config.Mail.auth.user,
-        pass: config.Mail.auth.pass
-    }
-});
+const transporter = nodemailer.createTransport(config.Mail);
 
+const key = 114514;
 sendMail({
     from: '"月月呀" <xiaoyue0782@163.com>',
     to: "1669044502@qq.com",
-    subject: "测试邮件",
-    text: "这是一封测试邮件",
-    html: "<p>这是一封测试邮件abc</p>"
+    subject: "QYServer | 邮箱绑定验证",
+    text: ([
+        "亲爱的玩家：",
+        "您好！",
+        "感谢您选择我们的服务！为了确保您的账户安全，请查收本次绑定邮箱的验证码：",
+        "",
+        `验证码：${key}`,
+        "有效期15分钟，请勿泄露给他人哦~",
+        "",
+        "如有任何疑问，欢迎随时联系我们。祝您使用愉快！",
+        "QYServer 团队"
+    ].join("\n")),
+    html: ([
+        "<div>亲爱的玩家：<br /></div>",
+        "<div>您好！</div>",
+        "<div>感谢您选择我们的服务！为了确保您的账户安全，请查收本次绑定邮箱的验证码：</div>",
+        "<div><br /></div>",
+        `<div><b>验证码：${key}</b></div>`,
+        "<div>有效期15分钟，请勿泄露给他人哦~</div>",
+        "<div><br /></div>",
+        "<div>如有任何疑问，欢迎随时联系我们。祝您使用愉快！</div>",
+        "<div>QYServer 团队</div>"
+    ].join(""))
 }, (res, isSend) => {
     logger.warn(JSON.stringify(res, (key, value) => {
         if (key === 'request' || key === 'config' || key === 'headers') return undefined;
@@ -24,6 +37,8 @@ sendMail({
         return value;
     }, 4));
 })
+
+
 
 ll.onUnload(() => { transporter.close() });
 async function sendMail(mailData, callback = (() => { })) {
