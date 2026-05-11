01a1472fe524cfb28ef16bcb8dc274ce42ebdf2a
Ice-rink <15551057621@163.com>
2026-05-11 12:18:27 +0000

    refactor: 优化末影龙伤害和水晶破坏逻辑

--- src/module/Game/EnderDragonEx.js
+++ src/module/Game/EnderDragonEx.js
@@ -1,7 +1,8 @@
 import * as func from "../../lib/func.js";
 
 const rystalTimeout = new Set();
-const playerDamage = {};
+const playerDamage = new Map();
+let inDamage = true;
 
 
 // === 代办 === //
@@ -23,32 +24,37 @@ const playerDamage = {};
 
 // 末影龙受伤
 mc.listen("onMobHurt", (mob, source, damage, cause) => {
-    if (mob.type === "minecraft:ender_dragon") {
-        if (cause === ActorDamageCause.BlockExplosion // 拦截爆炸伤害
-            || cause === ActorDamageCause.EntityExplosion
-        ) return false;
+    if (mob.type !== "minecraft:ender_dragon") return;
+    if (!inDamage) inDamage = true;
+    if (cause === ActorDamageCause.BlockExplosion // 拦截爆炸伤害
+        || cause === ActorDamageCause.EntityExplosion
+    ) return false;
+    if (source?.type !== "minecraft:player") return;
 
-        // 有45%的机率摇人
-        if (func.probability(30)) {
-            for (let i = 0; i < 5; i++) {
-                func.enRuncmd(mob, "execute as @a[r=500] at @s run structure load 末影侍卫 ~~~");
-            }
-        }
+    // 有45%的机率摇人
+    if (mob.health < 250 && func.probability(35)) {
+        func.enRuncmd(mob, "execute as @a[r=500] at @s run structure load 末影侍卫 ~~~");
     }
+
+    const xuid = source.toPlayer().xuid;
+    playerDamage.set(xuid, (playerDamage.get(xuid) ?? 0) + Math.ceil(damage));
 })
 
 // 玩家破坏水晶
 mc.listen("onAttackEntity", (player, entity) => {
-    if (!(entity.type === "ender_crystal"
+    if (!(inDamage
+        && entity.type === "minecraft:ender_crystal"
         && entity.pos.dimid === 2
     )) return;
 
     // 奖励破坏者debuff
-    [2, 9, 15, 17, 20].forEach(id => player.addEffect(id, 10, 1, true));
+    if (player.gameMode !== 1)
+        [2, 9, 15, 17, 20].forEach(id => player.addEffect(id, (5 * 20), 1, true));
 
     // 末影水晶自动恢复
+    const pos = entity.pos;
     const rystalDamage = setTimeout(() => {
-        mc.runcmdEx(`execute in the_end positioned ${func.pos2str(entity.pos)} run summon ender_crystal`);
+        mc.runcmdEx(`execute in the_end positioned ${func.pos2str(pos)} unless entity @e[r=2.5,type=ender_crystal] run summon ender_crystal`);
         rystalTimeout.delete(rystalDamage);
     }, 120 * 1000);
     rystalTimeout.add(rystalDamage);
@@ -56,9 +62,10 @@ mc.listen("onAttackEntity", (player, entity) => {
 
 
 
-// 生物被末影龙杀死
+// 玩家被末影龙杀死
 mc.listen("onMobDie", (entity, source) => {
-    if (!(source.type === "minecraft:ender_dragon"
+    if (!(inDamage
+        && source?.type === "minecraft:ender_dragon"
         && entity?.type === "minecraft:player"
     )) return;
     source.heal(10);
@@ -71,28 +78,35 @@ mc.listen("onMobDie", (mob) => {
 
     mc.setBlock(0, 64, 0, 2, "minecraft:dragon_egg");
     mc.runcmdEx("playsound mob.enderdragon.death @a");
+    mc.runcmdEx("execute in the_end run kill @e[x=0,y=64,z=0,r=500,type=ender_crystal]");
+    mc.runcmdEx("kill @e[tag=qys:末影侍卫]");
     rystalTimeout.clear();
+    inDamage = false;
 
     let index = 1;
-    playerDamageList
+    [...playerDamage]
         .sort((a, b) => a[1] - b[1])
-        .forEach(([damage, xuid]) => {
+        .forEach(([xuid, damage]) => {
             const player = mc.getPlayer(xuid) ?? null;
             const anking = 10 - index;
 
             if (func.isNull(player)) return;
 
             // 物品
-            player.giveItem("minecraft:netherite_ingot", anking * 5);
+            mc.runcmdEx(`give "${player.realName}" netherite_ingot ${anking * 5}`);
 
             // 经济奖励
             player.addScore("蜡烛", anking * 3000);
             player.addScore("金币", anking * 1000);
 
             // 称号
-            if (player.hasTag("tag:§r§l§5屠§d龙§b勇§3士§r")) {
+            if (!player.hasTag("tag:§r§l§5屠§d龙§b勇§3士§r")) {
                 func.enRuncmd(player, "function tag/tag4");
                 mc.broadcast(`${player.realName} 完成了进度 §d[解放末地]§r`);
+                player.tell(
+                    "§b§d最近的未探索过的末地城在：" 
+                    + mc.runcmdEx(`execute at "${player.realName}" run locate structure end_city true`).output
+                );
             }
 
             playerDamage.delete(xuid);
