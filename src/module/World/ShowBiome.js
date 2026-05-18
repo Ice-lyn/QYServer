2e41e8c0907b4cb8d18cb98ff1b9a46877e82651
Ice-rink <15551057621@163.com>
2026-05-18 09:21:29 +0000

    refactor: 优化生物群系名称处理

--- src/module/World/ShowBiome.js
+++ src/module/World/ShowBiome.js
@@ -6,11 +6,15 @@ mc.listen("onLeft", (pl) => playerBiome.delete(pl.xuid));
 setInterval(() => {
     mc.getOnlinePlayers().forEach(pl => {
         if (pl?.hasTag("qys:on_ShowBiome")) return;
-        const BiomeName = pl.getBiomeName();
-        if (playerBiome.get(pl.xuid) === BiomeName) return;
-        //pl.setTitle(`${BiomeNameCn.get(BiomeName,BiomeName)}`,2);
-        pl.setTitle(`§§\n\n\n\n\n\n${BiomeNameCn.get(BiomeName, BiomeName)}              §§`, 2);
-        //pl.setTitle("§8此提示为早期测试版本，未来可能会修改或删除",3);
-        playerBiome.set(pl.xuid, BiomeName);
+
+        let biomeName = pl.getBiomeName();
+        biomeName = biomeName.startsWith("minecraft:") // 高版本lse兼容
+            ? biomeName.slice(10)
+            : biomeName;
+
+        if (playerBiome.get(pl.xuid) === biomeName) return;
+        playerBiome.set(pl.xuid, biomeName);
+        
+        pl.setTitle(`§§\n\n\n\n\n\n${BiomeNameCn.get(biomeName, biomeName)}              §§`, 2);
     })
 }, 3000)
