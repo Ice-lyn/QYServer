b1830dd3968cb813a67cf00fd33f969fd7c48186
Ice-rink <15551057621@163.com>
2026-05-29 13:53:42 +0000

    refactor: 重构皮肤逻辑

--- src/module/Game/SkinEffect.js
+++ src/module/Game/SkinEffect.js
@@ -1,99 +1,125 @@
-// 皮肤特效配置
-const SkinEffect = new Map([
-    ["1", { // 小月
+import * as func from "../../lib/func.js";
+
+const beforeSkinMap = new Map();
+const skinEffectData = { // 皮肤特效配置
+    1: { // 小月
         add: [[1, 1]],
         remove: [1]
-    }],
-    ["2", { // 爱丽丝
+    },
+    2: { // 爱丽丝
         add: [[29, 10], [3, 2], [10, 2]],
         remove: [29, 3, 10]
-    }],
-    ["3", { // 猫猫
+    },
+    3: { // 猫猫
         add: [[1, 2], [16, 1]],
         remove: [1, 16]
-    }],
-    ["5", { // 莉莉丝
+    },
+    5: { // 莉莉丝
         add: [[5, 5], [3, 5], [16, 5]],
         remove: [5, 3, 16]
-    }],
-    ["6", { // 怕怕
+    },
+    6: { // 怕怕
         add: [[1, 2]],
         remove: [1]
-    }]
-])
+    }
+}
+
+// 玩家退出
+mc.listen("onLeft", (player) => beforeSkinMap.delete(player.xuid));
 
 // 玩家移除效果事件
-mc.listen("onRespawn", (pl) => {
-    const beforeSkin = pl.getAllTags().find(t => t.startsWith('qys:beforeSkin_'))?.split('_')[1] || null;
-    if (beforeSkin) return setSkinEffect(pl, beforeSkin, 1);
+mc.listen("onRespawn", (player) => {
+    const beforeSkin = getBeforeSkin(player);
+    if (beforeSkin) setSkinEffect(player, beforeSkin, 1);
+    else if (beforeSkin === -1) player.addEffect(14, 9999999, 1, false)
 })
 
 // 皮肤装备相关
-mc.listen("onSetArmor", (pl, slotNum, item) => {
-    if (!pl?.inWorld || !(slotNum === 1 || slotNum === 0)) return;
+mc.listen("onSetArmor", (player, slotNum, item) => {
+    if (!(player?.inWorld && (slotNum === 0 || slotNum === 1))) return;
+    const beforeSkin = getBeforeSkin(player);
+  
+    // 脱下
+    if (beforeSkin) {
+        if (beforeSkin === -1) {
+            player.removeEffect(14);
+            setBeforeSkin(player, beforeSkin, 0)
+        } else {
+            setBeforeSkin(player, beforeSkin, 0);
+            setSkinEffect(player, beforeSkin, 0);
+        }
+    }
 
-    const skinNum = item?.type?.match(/skin_item_(\d+)/)?.[1] || null;
-    const beforeSkin = pl.getAllTags().find(t => t.startsWith('qys:beforeSkin_'))?.split('_')[1] || null;
-    if (beforeSkin === null && skinNum === null) return;
-
-    if (skinNum !== null) { // 穿上
-        if (beforeSkin !== null) setSkinEffect(pl, beforeSkin, 0);
-        setSkinEffect(pl, skinNum, 1);
-    } else if (skinNum === null && beforeSkin !== null) {// 脱下
-        setSkinEffect(pl, beforeSkin, 0);
+    // 穿上
+    if (item?.type?.startsWith("qys:skin_")) {
+        if (item.type.startsWith("qys:skin_item_")) {
+            const skinNum = Number(item.type.match(/skin_item_(\d+)/)[1]);
+            setSkinEffect(player, skinNum);
+            setBeforeSkin(player, skinNum);
+        } else {
+            player.addEffect(14, 9999999, 1, false);
+            setBeforeSkin(player, -1);
+        }
     }
 })
 
+function getBeforeSkin(player) {
+    if (beforeSkinMap.has(player.xuid))
+        return beforeSkinMap.get(player.xuid);
 
-/**
- * 设置玩家使用皮肤的效果
- * @param {Player} pl - 目标玩家对象
- * @param {string} skinNum - 皮肤ID
- * @param {number} mode - 设置模式 (0:移除，1:添加)
- */
-function setSkinEffect(pl, skinNum, mode) {
-    const effect = SkinEffect.get(skinNum) || null;
-    skinNum = Number(skinNum);
-    if (mode) {
-        switch (skinNum) {
-            case 3:
-                pl.addTag("isCat");
-                break;
-            case 5:
-                mc.runcmdEx(`ability "${pl.realName}" mayfly true`);
-                break;
-            case 7:
-                mc.runcmdEx(`playanimation "${pl.realName}" animation.player.qys_riru none 1 "" riru`);
-                break;
-            case 8:
-                mc.runcmdEx(`playanimation "${pl.realName}" animation.player.qys_horror none 1 "" horror`);
-                break;
-        }
-        if (skinNum !== 8) pl.addEffect(14, 9999999, 1, false);
-        pl.addTag("qys:beforeSkin_" + skinNum);
-    } else {
-        switch (skinNum) {
-            case 3:
-                pl.removeTag("isCat");
-                break;
-            case 5:
-                mc.runcmdEx(`ability @a[name="${pl.realName}",m=!1,scores={飞行药水时长=..1}] mayfly false`);
-                break;
-            case 7:
-                mc.runcmdEx(`playanimation "${pl.realName}" animation.player.qys_recover none 0 "" riru`);
-                break;
-            case 8:
-                mc.runcmdEx(`playanimation "${pl.realName}" animation.player.qys_recover none 0 "" horror`);
-                break;
-        }
-        if (skinNum !== 8) pl.removeEffect(14);
-        pl.removeTag("qys:beforeSkin_" + skinNum);
+    const skinNum = Number(
+        player.getAllTags()
+            ?.find(t => t.startsWith('qys:beforeSkin_'))
+            ?.split('_')?.[1] || 0
+    );
+    beforeSkinMap.set(player.xuid, skinNum);
+    return skinNum;
+}
+
+function setBeforeSkin(player, skinNum, mode = 1) {
+    switch (mode) {
+        case 0:
+            player.removeTag("qys:beforeSkin_" + skinNum);
+            beforeSkinMap.delete(player.xuid);
+            break;
+        case 1:
+            player.addTag("qys:beforeSkin_" + skinNum);
+            beforeSkinMap.set(player.xuid, skinNum);
+            break;
     }
+}
 
-    if (effect === null) return;
-    const operation = mode ? effect.add : effect.remove;
-    operation.forEach(params => {
-        if (mode) pl.addEffect(params[0], 9999999, params[1], true);
-        else pl.removeEffect(params);
-    })
+function setSkinEffect(player, skinNum, mode = 1) {
+    if (!skinNum) return;
+    const effectData = skinEffectData[skinNum];
+  
+    if (mode) {
+        effectData?.add?.forEach(data => player.addEffect(data[0], 9999999, data[1], false));
+        if (skinNum !== 8) player.addEffect(14, 9999999, 1, false);
+        switch (skinNum) {
+            case 5:
+                mc.runcmdEx(`ability "${player.realName}" mayfly true`);
+                break;
+            case 7:
+                mc.runcmdEx(`playanimation "${player.realName}" animation.player.qys_riru none 1 "" riru`);
+                break;
+            case 8:
+                mc.runcmdEx(`playanimation "${player.realName}" animation.player.qys_horror none 1 "" horror`);
+                break;
+        }
+    } else {
+        effectData?.remove?.forEach(data => player.removeEffect(data));
+        if (skinNum !== 8) player.removeEffect(14);
+        switch (skinNum) {
+            case 5:
+                mc.runcmdEx(`ability @a[name="${player.realName}",m=!1,scores={飞行药水时长=..1}] mayfly false`);
+                break;
+            case 7:
+                mc.runcmdEx(`playanimation "${player.realName}" animation.player.qys_recover none 0 "" riru`);
+                break;
+            case 8:
+                mc.runcmdEx(`playanimation "${player.realName}" animation.player.qys_recover none 0 "" horror`);
+                break;
+        }
+    }
 }
