a21895ecc9e80e868da03806330a640eb84716ac
Ice-rink <15551057621@163.com>
2026-07-12 09:31:46 +0000

    feat: 添加MobSummon模块

--- src/module/load.js
+++ src/module/load.js
@@ -9,6 +9,7 @@ const lib_list = Object.entries({ // 我不管 反正好看 看的舒服
         // "AIPlayer.js",
         "JoinTime.js", // 加入时间相关
         "Mail.js", // 邮件系统
+        "MobSummon.js", // 实体生成
         "UserBind.js", // 用户绑定
         "UserMigrate.js" // 帐号迁移
     ],
