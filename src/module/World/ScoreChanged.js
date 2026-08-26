6857a850f6f34e384a40ea179a6128455b8d7cf0
Ice-lyn <fangfubin0782@gmail.com>
2026-08-26 19:53:01 +0000

    fix & feat: 修复玩家经济变更逻辑 并添加行为日志记录

--- src/module/World/ScoreChanged.js
+++ src/module/World/ScoreChanged.js
@@ -1,3 +1,4 @@
+import * as func from "../../lib/func.js";
 const scoreCache = new Map();
 
 mc.listen("onLeft", (pl) => scoreCache.delete(pl.xuid));
@@ -11,12 +12,13 @@ mc.listen("onJoin", (pl) => {
 mc.listen("onScoreChanged", (player, newScore, name) => {
     if (!(name === "蜡烛" || name === "金币")) return;
     
-    const cache = scoreCache.get(player.xuid);
+    const cache = scoreCache.get(player.xuid) ?? {};
     if (!cache) return;
     const delta = newScore - (cache[name] ?? 0);
 
     if (delta === 0) return;
     player.tell(`§e${name}§r ${`${delta}`[0] == "-" ? delta : "+" + delta}`, 5);
-    cache[name] = newScore;
-    const pos = new IntPos(player.pos.x, player.pos.y, player.pos.z, player.pos.dimid);
+    func.addBehaviorLog(2, "onScoreChanged", player.realName, player.pos,
+        `${name}经济变更：${`${delta}`[0] == "-" ? delta : "+" + delta}`
+    );
 })
