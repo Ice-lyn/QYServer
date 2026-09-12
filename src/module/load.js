511ee66ff754a57f4c231ee934c7e75d153f9f18
Ice-lyn <Icelyn0782@gmail.com>
2026-09-12 07:21:43 +0000

    refactor: 移除BoxUI

--- src/module/load.js
+++ src/module/load.js
@@ -23,7 +23,7 @@ const lib_list = Object.entries({ // 我不管 反正好看 看的舒服
      */
     Game: [
         "BlockLock.js", // 领地方块锁
-        "BoxUI.js", // 箱子UI
+        // "BoxUI.js", // 箱子UI
         "CloudLift.js", // 云朵电梯
         "Doll.js", // 玩偶
         "EnderDragonEx.js", // 更凶猛的末影龙
