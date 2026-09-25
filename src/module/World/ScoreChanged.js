915a8cabbd090b1f630bb9f78c1e222ff61ed456
Ice-lyn <Icelyn0782@gmail.com>
2026-09-25 11:01:16 +0000

    refactor: 优化分数缓存逻辑，使用对象替代Map

--- src/module/World/ScoreChanged.js
+++ src/module/World/ScoreChanged.js
@@ -1,23 +1,24 @@
-import * as func from "../../lib/func.js";
-const scoreCache = new Map();
+const scoreCache = {};
 
-mc.listen("onLeft", (pl) => scoreCache.delete(pl.xuid));
-mc.listen("onJoin", (pl) => {
-    scoreCache.set(pl.xuid, {
-        金币: pl.getScore("金币"),
-        蜡烛: pl.getScore("蜡烛")
-    });
+mc.listen("onLeft", (player) => delete scoreCache[player.xuid]);
+mc.listen("onJoin", (player) => {
+    scoreCache[player.xuid] = {
+        金币: player.getScore("金币"),
+        蜡烛: player.getScore("蜡烛")
+    };
 });
 
 mc.listen("onScoreChanged", (player, newScore, name) => {
     if (!(name === "蜡烛" || name === "金币")) return;
-    
-    const cache = scoreCache.get(player.xuid) ?? {};
-    if (!cache) return;
-    const delta = newScore - (cache[name] ?? 0);
 
+    const cache = scoreCache[player.xuid];
+    if (!cache) return;
+
+    const delta = newScore - (cache[name] ?? 0);
     if (delta === 0) return;
-    player.tell(`§e${name}§r ${`${delta}`[0] == "-" ? delta : "+" + delta}`, 5);
+
+    cache[name] = newScore;
+    player.tell(`§e${name}§r ${delta > 0 ? "+" : ""}${delta}`, 5);
     func.addBehaviorLog(2, "onScoreChanged", player.realName, player.pos,
         `${name}经济变更：${`${delta}`[0] == "-" ? delta : "+" + delta}`
     );
